9b22a73f32e83c884eedc3c26a05ce73914d7184
jyunyuk <wanyuli104@gmail.com>
2024-01-19 14:40:57 -0500

    secure credentials

--- analyze.py
+++ analyze.py
@@ -2,9 +2,11 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 import time
+import os
 
-endpoint = "ENTER ENDPOINT HERE"
-key = "ENTER KEY HERE"
+# Load endpoint and key from environment variables
+endpoint = os.environ.get('AZURE_COMPUTERVISION_ENDPOINT')
+key = os.environ.get('AZURE_COMPUTERVISION_KEY')
 
 credentials = CognitiveServicesCredentials(key)
 
